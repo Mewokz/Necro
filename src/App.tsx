a77510a8322dc569157134512a5ba04c2e9ae65a
Mewokz <mewokzwork@gmail.com>
2026-01-20 16:18:23 +0300

    Hot fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { AssetGate } from "./components/AssetGate";
 import { MainLayout } from "./components/layout/MainLayout";
-import { Analytics } from "@vercel/analytics/next";
 
 export default function App() {
   return (
     <AssetGate>
       <MainLayout />
-      <Analytics />
     </AssetGate>
   );
 }
