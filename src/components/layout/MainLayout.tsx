e3e51bba9beb9c1430d8deff4e6fbefb69d1063e
Mewokz <mewokzwork@gmail.com>
2026-01-20 16:16:52 +0300

    Hot fix

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,8 +5,7 @@ import { MediaGrid } from "./ui/MediaGrid";
 import type { MediaItem } from "./ui/types";
 import { ProximityDimHiss } from "./ui/ProximityDimHiss";
 import { UnstableDimmingWord } from "./ui/UnstableDimmingWord";
-import { Analytics } from "@vercel/analytics/next";
-<Analytics />;
+
 const GAMES: MediaItem[] = [
   {
     id: "doom",
